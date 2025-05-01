4357163dfa4d3eae66fe415f8a4a85c297a1e943
Satyam Vishwakarma <satyambro333@gmail.com>
2025-05-01 19:31:09 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <ul class="flex space-x-6">
    
    with the text content:
    
        HomeNotesQuestion PapersPracticalsE-BooksHome AssignmentsHall TicketResultAboutContact
    
    Relevant files:
    
    And change it as follows:
    
    haata do delet kar do

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,20 +67,7 @@ export default function RootLayout({
                 </Link>
                 {/* Desktop Navigation */}
                 <div className="hidden md:flex items-center space-x-6">
-                  <ul className="flex space-x-6">
-                    {navLinks.map((link) => (
-                      <li key={link.href}>
-                        <Link
-                          href={link.href}
-                          className="text-primary-foreground hover:opacity-80 transition-transform duration-300 transform hover:scale-110 inline-block" // Added hover scale animation
-                          target={link.external ? "_blank" : undefined}
-                          rel={link.external ? "noopener noreferrer" : undefined}
-                        >
-                          {link.label}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
+                   {/* Desktop navigation list removed */}
                   <ThemeToggle />
                 </div>
                 {/* Mobile Navigation Trigger */}
@@ -179,13 +166,13 @@ export default function RootLayout({
                        {/* Direct Contact Info */}
                        <li>
                          <a href="mailto:satyambro333@gmail.com" className="flex items-center text-sm hover:text-primary transition duration-300">
-                           <Mail size={14} className="mr-1" />
+                           <Mail size={16} className="mr-1" /> {/* Consistent icon size */}
                            satyambro333@gmail.com
                          </a>
                        </li>
                        <li>
                          <a href="tel:+917039201803" className="flex items-center text-sm hover:text-primary transition duration-300">
-                           <Phone size={14} className="mr-1" />
+                           <Phone size={16} className="mr-1" /> {/* Consistent icon size */}
                            +91 7039201803
                          </a>
                        </li>
@@ -197,7 +184,7 @@ export default function RootLayout({
                             aria-label="WhatsApp"
                             className="flex items-center text-sm hover:text-primary transition duration-300"
                           >
-                           <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="currentColor" className="mr-1"><path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.894 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.5-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.626.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z"/></svg>
+                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor" className="mr-1"><path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.894 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.5-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.626.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z"/></svg>
                             WhatsApp
                           </a>
                       </li>
